3d9581f5c32711843c5def94827578cada4c9f02
healthylife1191 <159755852+healthylife1191@users.noreply.github.com>
2024-03-09 09:10:44 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,29 @@
+    });
+    data.forEach(item => {
+}
+    const data = [];
+function main() {
+    for (let i = 0; i < 10; i++) {
+
+function generateRandomData() {
+        console.log("Random Number: " + item);
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    }
+}
+
+    const data = generateRandomData();
+    return data;    public static void main(String[] args) {
+        }
+
+    }
+public class RandomDataGenerator {
+        int[] data = new int[10];
+
+        }
+}
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
